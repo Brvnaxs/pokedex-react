11a12c379e6270963dbca758dabda19e00ba1c0e
marinhowilliam <adelton.william07@gmail.com>
2023-04-19 20:58:41 -0300

    Adiciona estilos aos cards

--- src/components/list-pokemons/List.jsx
+++ src/components/list-pokemons/List.jsx
@@ -7,9 +7,17 @@ export default function List(){
         fetch('https://pokeapi.co/api/v2/pokemon')
         .then(res => res.json())
         .then(res => {
-            setPokemonsList(res.results)
+            res.results.forEach(({name, url}) => {
+                fetch(url)
+                .then(res => res.json())
+                .then(pokemon => {
+                    const poke = {'name': pokemon.forms[0].name,'image': pokemon.sprites.other.dream_world.front_default}
+                    setPokemonsList( (current) => [poke, ...current])
+                    console.log(poke)
+                })
+            });
         })
-    })
+    }, [])
 
     return(
         <div className='List'>
@@ -17,8 +25,9 @@ export default function List(){
                 pokemonsList.map(pokemon =>{
                     return(
                         <div className='poke-card' key={pokemon.name}>
+                            <img src={pokemon.image} alt={pokemon.name} />
                             <h1>{pokemon.name}</h1>
-                            <button>Saiba mais</button>
+                            <button>Informações</button>
                         </div>
                     )
                 })
